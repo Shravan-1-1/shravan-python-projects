6c174826ccc3c0e96ea0cae10ded500dcdfcf0d4
Shravan Kumar Sharma <shravan.a6b@gmail.com>
2026-02-27 11:17:31 +0530

    Day 6 Problem 2: Portfolio script + requirements.txt complete

---
+++ shravan_portfolio.py
@@ -0,0 +1,5 @@
+import streamlit as st
+st.title("Shravan Python Developer Portfolio")
+st.markdown("""
+**Prayagraj, UP | B.Tech CSE | Govt Job + AI Aspirant**
+**GitHub**: https://github.com/Shravan-1-1/shravan-python-projects
